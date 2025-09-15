04641ce56909ed60a0b9e328db3d9b921c46095c
n-tomoki <izumi@fairy.memail.jp>
2025-09-15 14:20:28 +0900

    最初のコミット

---
+++ DlgMain.h
@@ -0,0 +1,38 @@
+﻿
+// DlgMain.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CDlgMain ダイアログ
+class CDlgMain : public CDialogEx
+{
+// コンストラクション
+public:
+	CDlgMain(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MOVEJPEG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnBnClickedButtonQuit();
+};
---
+++ MoveJpeg.h
@@ -0,0 +1,32 @@
+﻿
+// MoveJpeg.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CApp:
+// このクラスの実装については、MoveJpeg.cpp を参照してください
+//
+
+class CApp : public CWinApp
+{
+public:
+	CApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CApp theApp;
